5fe653be774f9980cf37b27fd151afda3ca3c206
Yerobeam <yerobeam@del.ac.id>
2026-07-26 20:16:59 +0700

    Bersihkan sisa ngrok dan shared hosting InfinityFree

--- clear.php
+++
@@ -1,7 +0,0 @@
-   <?php
-   require __DIR__.'/../vendor/autoload.php';
-   $app = require_once __DIR__.'/../bootstrap/app.php';
-   $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-   $kernel->call('config:clear');
-   $kernel->call('cache:clear');
-   echo 'Done';
