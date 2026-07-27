600510581834f0b151ad4fd767d686685df6ba31
Yerobeam <yerobeam@del.ac.id>
2026-07-28 04:10:51 +0700

    GSC

--- resources/views/modules/landing/layouts/app.blade.php
+++ resources/views/modules/landing/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="I0f3Dx6hcE_3I0pMKYB26tDyNuw0oTGzAzOTrk-sI7o" />
     <title>@yield('title', 'Beranda - PKK Kabupaten Toba')</title>
 
     {{-- Favicon --}}
